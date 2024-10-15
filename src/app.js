f492f8b3264f05b0f4be9e10d195ede65b28fae5
Owillz01 <omonkhodion@gmail.com>
2024-10-15 10:46:09 +0100

    done with the part 2 of the exercise

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
+app.use(express.json())
+app.use(express.urlencoded());
+app.get('/restaurants', async (req, res) => {
+    let restaurants = await Restaurant.findAll()
+    res.json(restaurants)
+})
+
+app.get("/restaurants/:id", async (req, res) => {
+  let restaurant = await Restaurant.findByPk(req.params.id);
+  res.json(restaurant);
+});
+
+app.post("/restaurants/", async (req, res) => {
+    let arams = req.body
+    console.log(arams, 'PARAMS');
+    
+  let restaurant = await Restaurant.create(req.params);
+  res.json(restaurant);
+});
 
 
+app.put("/restaurants/:id", async (req, res) => {
+  let data = req.body;
 
+  let restaurant = await Restaurant.findByPk(req.params.id);
+  let resData = await restaurant.update(data)
+  res.json(resData);
+});
+
+app.delete("/restaurants/:id", async (req, res) => {
+  let restaurant = await Restaurant.findByPk(req.params.id);
+  let resData = await restaurant.destroy();
+  res.json(resData);
+});
 
 module.exports = app;
