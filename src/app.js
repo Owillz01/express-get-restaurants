7cb08cfb62fa6bd86246d6a9f7055a2cad6ad7fe
Owillz01 <omonkhodion@gmail.com>
2024-10-16 18:05:29 +0100

    done with the exras for today

--- src/app.js
+++ src/app.js
@@ -2,41 +2,12 @@ const express = require("express");
 const app = express();
 const Restaurant = require("../models/index")
 const db = require("../db/connection");
+const restaurantRoutes = require("../routes/restaurants");
 
 //TODO: Create your GET Request Route Below: 
 app.use(express.json())
 app.use(express.urlencoded());
-app.get('/restaurants', async (req, res) => {
-    let restaurants = await Restaurant.findAll()
-    res.json(restaurants)
-})
+app.use("/restaurants", restaurantRoutes);
 
-app.get("/restaurants/:id", async (req, res) => {
-  let restaurant = await Restaurant.findByPk(req.params.id);
-  res.json(restaurant);
-});
-
-app.post("/restaurants/", async (req, res) => {
-    let arams = req.body
-    console.log(arams, 'PARAMS');
-    
-  let restaurant = await Restaurant.create(req.params);
-  res.json(restaurant);
-});
-
-
-app.put("/restaurants/:id", async (req, res) => {
-  let data = req.body;
-
-  let restaurant = await Restaurant.findByPk(req.params.id);
-  let resData = await restaurant.update(data)
-  res.json(resData);
-});
-
-app.delete("/restaurants/:id", async (req, res) => {
-  let restaurant = await Restaurant.findByPk(req.params.id);
-  let resData = await restaurant.destroy();
-  res.json(resData);
-});
 
 module.exports = app;
